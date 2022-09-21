6f177c443f3eed1078b274802229114db64e72d4
sarthakbhardwaj27 <85218961+sarthakbhardwaj27@users.noreply.github.com>
2022-09-21 23:01:53 +0530

    Create intro.py

---
+++ Time Complexities/intro.py
@@ -0,0 +1,32 @@
+there are three types od asymptotic notations: 
+  1. Big O 
+  2. Omega
+  3. Theta
+ 
+Big O: 
+  it gives the WORST time complexity of te algo
+  it defines if the set of functions is going to grow SLOWER than or at the same rate with respect to the expression
+  this gives the MAXIMUM amount of time required by an algo 
+  
+Omega: 
+  it defines if the set of functions will grow FASTER or at the same rate with respect to the expression
+  this gives the BEST case of an algo's time complexity
+  this gives the MINIMUM time required by the algo
+  
+Theta:
+  this is used to define if the function is going to lie between Omega and Big O. 
+  this is used to find the AVERAGE time reqd. 
+  
+  
+ TIME COMPLEXITY OF SORTING ALGOS: 
+  1. INSERTION SORT: O(n^2) and omega(n) 
+    for very small "n" insertion sort is fastest as compared to other best ones
+  2. MERGE SORT: O(n^2) and omega(nlogn)
+    quicksort is a divide and conquer algo 
+    FASTEST sorting algo 
+  3. BUBBLE SORT: O(n^2) and omega(n)
+    simplest sorting algo
+    
+ TIME COMPLEXITY OF SEARCHING ALGOS:
+  
+    
