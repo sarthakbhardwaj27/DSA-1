008f8de4e0b1a2759a1431744d58fbcd21406076
Sarthak Bhardwaj <42564358+Sarthakbhardwaj@users.noreply.github.com>
2022-10-05 00:52:35 +0530

    Update intro.py

--- Tree/intro.py
+++ Tree/intro.py
@@ -16,16 +16,31 @@ TYPES OF TREES:
   4. binary search tree
   5. avl trees:  It can be considered as a binary tree and also a type of binary search tree. It satisfies features of both binary tree and binary search tree.
 It's a self balancing tree i.e. balancing heights of left subtree and right subtree. This balancing is measured by something called balancing factor.
-A tree is considered as an AVL tree if it satisfies properties of both a binary search tree and the balancing factor. Difference between the height of the left subtree and the right subtree is considered as the height of the AVL tree.The value of the balancing factor must be 0, 1 or -1 for each node in an AVL tree.
-  
+A tree is considered as an AVL tree if it satisfies properties of both a binary search tree and the balancing factor. 
+Difference between the height of the left subtree and the right subtree is considered as the height of the AVL tree.The value of the balancing factor
+must be 0, 1 or -1 for each node in an AVL tree.
   6. b-tree
-  7. red-black tree: It is also a variant of a binary search tree. It's also a self balancing tree just like an AVL tree, the only difference is in an AVL tree, we do not have an idea as to how much rotations would be required to balance the tree but in a red black tree a maximum of two rotations are required to 
+  7. red-black tree: It is also a variant of a binary search tree. It's also a self balancing tree just like an AVL tree, the only difference is in an AVL tree, 
+    we do not have an idea as to how much rotations would be required to balance the tree but in a red black tree a maximum of two rotations are required to 
     balance the tree. It contains a bit that represents the red or black color of the node to ensure the balancing of the tree.
   
 APPLICATIONS: 
-  1. traversing
-  2. searching
-  3. decision making
+  1. traversing - for example:folders in my computer 
+  2. searching - 
+  3. decision making - for example in netflix/prime, movie selection -> action or movies -> then movie 1 or movie 2
   4. machine learning
   
-
+TYPES OF TREES:
+  1. General tree
+  2. Binary Tree
+  3. Binary Search Tree
+  4. AVL tress: it can be considered as a binary tree and also a type of BST. it satisfies features 
+    of both BT nad BST. it is a self balancing (balancing heihgts of left subtree and right subtree. 
+    this balancing is measured by something called balacing factor. the idfference between the heights of the left and right subtree should be 0,1 or -1 for each node in a n AVL tree
+                                            
+  5.  Red-black tree: it is also a variant of a binary search tree. it is also a self balancing 
+      tree just like an AVL tree, the only difference is in an AVL tree, we do not have an idea 
+      as to how much rotations would be required to balance the tree but in a red black tree a 
+      mazimum of two rotations are required to balance the tree. it contains a bit that 
+      represents the red or black color of the node to ensure the balacing of the tree. 
+                                                
