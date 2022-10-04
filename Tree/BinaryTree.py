aeb89cfec3b35eccccc0b666ebbeacea4601850b
Sarthak Bhardwaj <42564358+Sarthakbhardwaj@users.noreply.github.com>
2022-10-05 01:53:43 +0530

    Create BinaryTree.py

---
+++ Tree/BinaryTree.py
@@ -0,0 +1,28 @@
+If there are n nodes in a perfect binary tree, and its height is given by "logn". 
+At every level the number of child nodes double so that is why
+
+The minimum number of nodes possible at the height h of a binary tree is given by h+1
+
+If the BT has L number of leaf nodes, then height is given by L+1
+
+At each level i of a BT, the number of total nodes is given by 2^i
+
+In full binary tree, if there are n number of total nodes: 
+  the number of internal nodes is given by (n-1)/2
+  the number of leaf nodes is given by (n+1)/2
+  
+COMPLETE BINARY TREE: 
+  a complete binary tree is a BT in which all the elements are aaranges without missing any sequence
+  all the levels are completely filled except the last level that may or may not be completely filled
+
+PERFECT BINARY TREE: 
+  if in a tree all the internal nodes have exactly two children nodes, 
+  all the leaf nodes are on the same level
+  a perfect binary tree with height h, the total number of nodes in this case is given by 2h-1
+  
+DEGENERATE BINARY TREE:
+  if in a binary tree each node contains only one child node either on the left side or the right side of the tree
+  they are equal to linked lists in terms of performance
+  
+
+
