d7af36e6125c31b3d07fc330c24c348bdc049778
Sarthak Bhardwaj <42564358+Sarthakbhardwaj@users.noreply.github.com>
2022-10-05 21:37:38 +0530

    Create BinarySearchTree.py

---
+++ Tree/BinarySearchTree.py
@@ -0,0 +1,38 @@
+A BST is a sorted binary tree, where we can easily search for any key using Binary Search algo. 
+
+To sort a BST the node's left subtree contains elements smaller than the node's key 
+
+------------------------COMPLEXITIES-----------------------
+Insertion: o(n)
+Searching: o(height)
+Deletion: o(n)
+  
+  
+WHY DO WE NEED BINARY SEARCH TREES?
+
+You: Wait!!!!! But Binary Search is already a super awesome searching algorithm. Why do we even need this so-called BST?
+
+Rahul: Great question! Yes Binary Search is a perfect algorithm, correction: almost perfect 🙂 But it works only on a fixed sorted data set. If your data set is not sorted, or elements are added/removed from it, do you think Binary Search would perform well?
+
+You: Ummmmmmm……..
+
+Rahul: Let’s try to understand this with an example.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
