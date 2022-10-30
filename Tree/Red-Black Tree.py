5fe0dddcaacb42c5c503fc54ddd6d9349161e8d8
Sarthak Bhardwaj <42564358+Sarthakbhardwaj@users.noreply.github.com>
2022-10-30 12:07:00 +0530

    Create Red-Black Tree.py

---
+++ Tree/Red-Black Tree.py
@@ -0,0 +1,4 @@
+a red black tree is a type of self-balancing binary search tree. 
+it usees an additional attribute to denote the colour of its nodes )either red or black)
+- when a tree is modified by inserting or deleting nodes, the tree is often rotated and recoloured to 
+ensure logarithmix time complexity for insert, search and delete operations 
